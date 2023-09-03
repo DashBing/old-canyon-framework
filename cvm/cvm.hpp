13ba5de5fc568fbe6140262247229b8aa278d697
DashBing <mcbbkf@outlook.com>
2023-09-03 16:46:57 +0800

    INIT

---
+++ cvm/cvm.hpp
@@ -0,0 +1,18 @@
+#ifndef _CVM_HPP
+#define _CVM_HPP
+
+#pragma pack(1)
+
+struct command{
+    unsigned long long cmd;
+};
+
+#pragma pack()
+
+class VM{
+    public:
+    
+};
+
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(int args, char * argv){
+    if(args > 0){
+    }
+    else{
+        cout << "ERROR! NO ARGS!" << endl;
+    }
+    return(0);
+}
+//电脑微信无响应了艹 你刚说的什么
+//e
+// 你听到多少
+// 哦没看
+//等一下，我把网页仓库拉过来
+//额，就是
+//你说什么单分支双分支就无响应了 我特么在聊天区发了 你没看
