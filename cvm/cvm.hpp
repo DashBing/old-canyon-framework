37efd0c28e553a6fbed3047f8076b52ce4e92337
DashBing <mcbbkf@outlook.com>
2023-09-05 12:46:32 +0800

    update

--- cvm/cvm.hpp
+++ cvm/cvm.hpp
@@ -1,20 +1,23 @@
+#include "types.h"
+
 #ifndef _CVM_HPP
 #define _CVM_HPP
 
 #pragma pack(1)
 union cmddata{  // 共用区域定义
-    unsigned short res;       // 寄存器地址
-    unsigned long long mem;   // 内存地址
-    unsigned long long data;  // 数据
+    UINT16 res;       // 寄存器地址
+    UINT64 mem;   // 内存地址
+    UINT64 data;  // 数据
 };
 
 struct command{
-    unsigned short cmd;  // 命令
-    unsigned short res;  // 寄存器地址
+    UINT16 cmd;  // 命令
+    UINT16 res;  // 寄存器地址
     cmddata data;        // 寄存器地址2||内存地址||数据 共用一块区域
 };
 #pragma pack()
 
+template <UINT64 mem_size>
 class VM{
     public:
     
---
+++ cvm/types.h
@@ -0,0 +1,9 @@
+#ifndef _TYPES_H
+#define _TYPES_H
+
+typedef unsigned char UINTB1, UINT8;
+typedef unsigned short UINTB2, UINT16;
+typedef unsigned int UINTB4, UINT32;
+typedef unsigned long long UINTB8, UINT64;
+
+#endif
