5de8ac503a66a0202f6f0b4963bfc71b18811ae3
DashBing <mcbbkf@outlook.com>
2023-09-05 12:35:52 +0800

    update

--- cvm/cvm.hpp
+++ cvm/cvm.hpp
@@ -2,17 +2,22 @@
 #define _CVM_HPP
 
 #pragma pack(1)
-
-struct command{
-    unsigned long long cmd;
+union cmddata{  // 共用区域定义
+    unsigned short res;       // 寄存器地址
+    unsigned long long mem;   // 内存地址
+    unsigned long long data;  // 数据
 };
 
+struct command{
+    unsigned short cmd;  // 命令
+    unsigned short res;  // 寄存器地址
+    cmddata data;        // 寄存器地址2||内存地址||数据 共用一块区域
+};
 #pragma pack()
 
 class VM{
     public:
     
 };
-
 
 #endif
