5d967a490e99e2ce969c7246aa944a87cfdb7523
DashBing <mcbbkf@outlook.com>
2023-09-06 12:26:28 +0800

    update

---
+++ cvm/cmddef.h
@@ -0,0 +1,8 @@
+#ifndef _CVM_CMDDEF_H
+#define _CVM_CMDDEF_H
+
+typedef enum command_index{
+    mov = 0x1
+} cmd_index;
+
+#endif
--- cvm/datatypedef.h
+++ cvm/datatypedef.h
@@ -4,20 +4,21 @@
 #define _CVM_DATATYPEDEF_H
 #pragma pack(1)
 
-union cmddata{  // 共用区域定义
-    UINT16 res;       // 寄存器地址
-    UINT64 mem;   // 内存地址
-    UINT8 data8;
-    UINT16 data16;
-    UINT32 data32;
-    UINT64 data64;  // 数据
-};
+typedef struct cmd_two_res{
+    UINT16 a;
+    UINT16 b;
+} cmd_resAB;
 
-struct command{
+typedef union cmd_union_data{  // 共用区域定义
+    cmd_two_res res;   // 寄存器地址
+    UINT64 mem;   // 内存地址
+    UINT64 data;  // 数据
+} cmd_data;
+
+typedef struct command_data{
     UINT16 cmd;  // 命令
-    UINT16 res;  // 寄存器地址
-    cmddata data;        // 寄存器地址2||内存地址||数据 共用一块区域
-};
+    cmd_union_data data;        // 寄存器地址2||内存地址||数据 共用一块区域
+} command;
 
 #pragma pack()
 #endif
