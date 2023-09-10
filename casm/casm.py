9d9027b7044582a6be2f96ab0501ebf93dd29b46
DashBing <mcbbkf@outlook.com>
2023-09-10 11:57:25 +0800

    update casm

---
+++ casm/casm.py
@@ -0,0 +1,25 @@
+def precom(code:str) -> list[list[str, ], ]:  # 按照换行分隔 并删除注释 再进行语句分隔
+    code.replace("\r", "\n")
+    code = code.split("\n")
+    l = []
+    for i in range(len(code)):
+        code[i] = code[i].split(";")[0]
+        if code[i] == "":
+            l.append(i)
+    for i in l:
+        del code[i]
+    for i in range(len(code)):
+        l = []
+        code[i] = code[i].split(" ")
+        for j in range(len(code[i])):
+            if code[i][j] == "":
+                l.append(j)
+        for j in l:
+            del code[i][j]
+    return(code)
+
+def tobytes(code:list) -> bytearray:
+    pass
+
+def compile(code:str) -> bytearray:
+    pass
---
+++ casm/main.py
@@ -0,0 +1,16 @@
+from sys import argv
+from casm import *
+
+def main(args:list):
+    a = ""
+    argt = args
+    del argt[-1]
+    for i in argt:
+        with open(i, "r") as f:
+            r = f.read()
+            r = compile(r)
+            a = a + r
+
+if __name__ == "__main__":
+    del argv[0]
+    main(argv)
