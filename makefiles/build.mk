5bb0e0b5a36b8eab293cdc8699fdee0acb7f33d1
DashBing <mcbbkf@outlook.com>
2023-09-16 13:46:09 +0800

    update build.mk

--- makefiles/build.mk
+++ makefiles/build.mk
@@ -7,7 +7,12 @@ endif
 BUILD_DIR := build
 
 build:
-	make build-cvm-runner
+	@make build-cvm-runner
+
+.PHONY:make-build-dir
+make-build-dir:
+	mkdir $(BUILD_DIR)
 
 build-cvm-runner:
+	@make make-build-dir
 	g++ cvm-runner/main.cpp -o __build__/cvm-runner$(FILE_OUT_TYPE)
