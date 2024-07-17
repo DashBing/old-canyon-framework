f2a2b66e6e919e1d6482939f43702df495e2b648
DashBing <mcbbkf@outlook.com>
2024-07-17 21:36:23 +0800

    change type defines

--- src/cvm/types.h
+++ src/cvm/types.h
@@ -1,9 +1,13 @@
 #ifndef _CVM_TYPES_H
 #define _CVM_TYPES_H
 
-typedef unsigned char UINTB1, UINT8;
-typedef unsigned short UINTB2, UINT16;
-typedef unsigned int UINTB4, UINT32;
-typedef unsigned long long UINTB8, UINT64;
+#include <stdint.h>
+
+namespace canyon{
+    typedef uint8_t UINTB1, UINT8;
+    typedef uint16_t UINTB2, UINT16;
+    typedef uint32_t UINTB4, UINT32;
+    typedef uint64_t UINTB8, UINT64;
+}
 
 #endif
